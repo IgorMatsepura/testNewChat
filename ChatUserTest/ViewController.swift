fb28afa5ac88811802b7ba51ab12839b42b1f5e7
Igor <igor.matsepura@gmail.com>
2021-02-22 13:21:47 +0200

    ChatInUsers ver.1

---
+++ ChatUserTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatUserTest
+//
+//  Created by IgorMac on 2/12/21.
+//  Copyright © 2021 IgorMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
